cfb85ec70b5ccae2218a418efef0d2dbd665b0cd
Stefan Mitić <mitkeprepecenica@gmail.com>
2016-05-30 15:03:05 +0200

    Project Initialization

---
+++ PracticeConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public int SumMultipliesOfNumbers()
+        {
+            int result = 0;
+            return result;
+        }
+    }
+}
